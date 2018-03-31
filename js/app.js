d146ecf5aa6dee5621565e3cc972623dd6657af6
brettghays <brett.g.hays.coding@gmail.com>
2018-03-31 09:12:24 -0600

    day 1 done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-angular.module("myApp", []);
+var app = angular.module('myApp', [])
+
+app.controller('mainCtrl', function($scope) {
+    $scope.message = 'Hello Utah!'  
+    $scope.todos = [
+      {
+        status: 'done',
+        title: 'clean socks'
+      },
+      {
+        status: 'not done',
+        title: 'make a smoothie'
+      }
+    ]
+    $scope.log = item => console.log(item);
+    $scope.addToDo = newToDo => $scope.todos.push({title: newToDo, status: 'not done'});
+    $scope.done = index => $scope.todos[index].status = 'done';
+    $scope.delete = index => $scope.todos.splice(index,1);
+});
+
+   
